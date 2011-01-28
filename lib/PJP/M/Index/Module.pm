c7d9663785e15152d7f7c233b27aa7d52ed3ca64
Tokuhiro Matsuno <tokuhirom@gmail.com>
2011-01-28 09:18:57 +0900

    redesign

---
+++ lib/PJP/M/Index/Module.pm
@@ -0,0 +1,21 @@
+use strict;
+use warnings;
+use utf8;
+
+package PJP::M::Index::Module;
+
+sub get {
+    my ($class, $c) = @_;
+
+    my @mods;
+    opendir my $dh, 'assets/perldoc.jp/docs/modules/';
+    while (defined(my $e = readdir $dh)) {
+        next if $e =~ /^\./;
+        next if $e =~ /^CVS$/;
+        push @mods, $e;
+    }
+    return @mods;
+}
+
+1;
+
