621333955fad493a1f4b4d2c62507031f05fd69d
Tokuhiro Matsuno <tokuhirom@gmail.com>
2011-02-02 22:00:12 +0900

    added better routing

--- lib/PJP/Web/Dispatcher.pm
+++ lib/PJP/Web/Dispatcher.pm
@@ -117,4 +117,29 @@ get '/docs{path:/|/.+}' => sub {
     }
 };
 
+get '/perl*' => sub {
+    my ($c, $p) = @_;
+    my ($splat) = @{$p->{splat}};
+    return $c->redirect("/pod/perl$splat");
+};
+
+my $re = join('|', qw(
+  abs accept alarm atan bind binmode bless break caller chdir chmod chomp chop chown chr chroot close closedir connect
+  continue cos crypt dbmclose dbmopen defined delete die do dump each endgrent endhostent endnetent endprotoent endpwent
+  endservent eof eval bynumber getprotoent getpwent getpwnam getpwuid getservbyname getservbyport getservent
+  getsockname getsockopt glob gmtime goto grep hex import index int ioctl join keys kill last lc lcfirst length link
+  listen local localtime lock log lstat m map mkdir msgctl msgget msgrcv msgsnd my next no oct open opendir ord order
+  our pack package pipe pop pos precision print printf prototype push q qq qr quotemeta qw qx rand read readdir readline
+  readlink readpipe recv redo ref rename require reset return reverse rewinddir rindex rmdir s say scalar seek seekdir select
+  semctl semget semop send setgrent sethostent setnetent setpgrp setpriority setprotoent setpwent setservent setsockopt shift
+  shmctl shmget shmread shmwrite shutdown sin size sleep socket socketpair sort splice split sprintf sqrt srand stat state
+  study sub substr symlink syscall sysopen sysread sysseek system syswrite tell telldir tie tied time times tr truncate uc
+  ucfirst umask undef unlink unpack unshift untie use utime values vec vector wait waitpid wantarray warn write y
+));
+get "/{name:$re}" => sub {
+    my ($c, $p) = @_;
+
+    return $c->redirect("/func/$p->{name}");
+};
+
 1;
