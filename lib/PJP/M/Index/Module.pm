473348f6a8e51f4d26944f23a6890f35a086011e
Tokuhiro Matsuno <tokuhirom@gmail.com>
2011-02-12 14:59:06 +0900

    oops

--- lib/PJP/M/Index/Module.pm
+++ lib/PJP/M/Index/Module.pm
@@ -10,7 +10,6 @@ use LWP::UserAgent;
 use CPAN::DistnameInfo;
 use Log::Minimal;
 use URI::Escape qw/uri_escape/;
-use JSON;
 use File::Spec::Functions qw/catfile/;
 use File::Find::Rule;
 use version;
