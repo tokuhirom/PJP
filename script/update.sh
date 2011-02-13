4ae9d17f86e327a43cfe055e33026a1ac9b0c345
Tokuhiro Matsuno <tokuhirom@gmail.com>
2011-02-13 11:38:42 +0900

    updated batch script

--- script/update.sh
+++ script/update.sh
@@ -10,3 +10,6 @@ rm /usr/local/webapp/PJP/assets/index-module.pl
 cd /usr/local/webapp/PJP
 perl -Ilib -e 'use PJP::M::Index::Module; use PJP; my $c = PJP->bootstrap; PJP::M::Index::Module->generate_and_save($c)'
 
+# 組み込み関数
+perl -Ilib -e 'use PJP; use PJP::M::BuiltinFunction; my $c = PJP->bootstrap; PJP::M::BuiltinFunction->generate($c)'
+
