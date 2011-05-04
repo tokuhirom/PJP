1613d21940423e0157eaa9ea60f0fc6c97589ce5
Tokuhiro Matsuno <tokuhirom@gmail.com>
2011-05-05 08:38:28 +0900

    dotcloudfy

--- script/update.sh
+++ script/update.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
-cd /usr/local/webapp/PJP/assets/perldoc.jp
+BASE=/home/dotcloud/code
+
+cd $BASE/assets/perldoc.jp
 cvs upd -dP
 
-cd /usr/local/webapp/PJP/assets/module-pod-jp
+cd $BASE/PJP/assets/module-pod-jp
 git pull origin master
 
-rm /usr/local/webapp/PJP/assets/index-module.pl
+rm $BASE/assets/index-module.pl
 
-cd /usr/local/webapp/PJP
+cd $BASE
 perl -Ilib -e 'use PJP::M::Index::Module; use PJP; my $c = PJP->bootstrap; PJP::M::Index::Module->generate_and_save($c)'
 
 # 組み込み関数
