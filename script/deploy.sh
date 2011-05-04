2e2079e3b395ba7bfa452c0eb015c1b3b6803406
Tokuhiro Matsuno <tokuhirom@gmail.com>
2011-05-05 07:50:42 +0900

    these scripts are no longer needed.

--- script/deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/zsh
-git push origin master
-ssh 64p.org -p 2222 "cd /usr/local/webapp/PJP/; git pull origin master; sudo /usr/local/webapp/PJP/script/restart.sh"
--- script/restart.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-/usr/bin/svc -t /service/pjp/
